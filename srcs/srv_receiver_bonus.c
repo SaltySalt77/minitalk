65d38d113072c33eb3d99849236d5f843b33d6c4
Hyeon Na <hyna@c5r8s1.42seoul.kr>
2022-07-10 17:10:56 +0900

    handeled error end d

--- srcs/srv_receiver_bonus.c
+++ srcs/srv_receiver_bonus.c
@@ -6,7 +6,7 @@
 /*   By: hyna <hyna@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 10:22:36 by hyna              #+#    #+#             */
-/*   Updated: 2022/07/10 17:09:32 by hyna             ###   ########.fr       */
+/*   Updated: 2022/07/10 16:56:16 by hyna             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,12 +32,23 @@ static void	put_message(char	*message, int	*idx, int *count, char	*letter)
 	*idx = 0;
 }
 
+static int unicode_handeler(unsigned char	letter)
+{
+	if (letter >= 240)
+		return (3);
+	else if (letter >= 224)
+		return (2);
+	else
+		return (1);
+}
+
 void	receive_processor(int signo, siginfo_t	*info, void	*context)
 {
-	static char	message[101];
+	static char	message[103];
 	static int	idx;
 	static int	count;
 	static char	letter;
+	static int	flag = 100;
 
 	(void) info;
 	(void) context;
@@ -46,12 +57,17 @@ void	receive_processor(int signo, siginfo_t	*info, void	*context)
 	count++;
 	if (letter == 127)
 		put_message(message, &idx, &count, &letter);
+	else if ((unsigned char)letter > 191 && idx > 90)
+		flag = idx + unicode_handeler(letter);
 	if (count == 8)
 	{
 		message[idx++] = letter;
 		refresh(NULL, &count, &letter);
 	}
-	if (idx == 100)
+	if (idx == 100 || idx == flag)
+	{
 		put_message(message, &idx, &count, &letter);
+		flag = 100;
+	}
 	letter <<= 1;
 }
